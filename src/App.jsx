4079aeb6905e6fdfc81bc37da48c7c5469bf4ba6
John Lester Pansoy <jlesterpansoy@gmail.com>
2025-04-23 12:19:03 +0800

    refactor: use lazy import for React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
